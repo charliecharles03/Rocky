239158a9728cbc9945c4aa86dbee7bac9893636a
Anilkumar Kumar <anilkumar.chenchu@dat.com>
2025-04-03 17:00:30 +0530

    added in cmake really good add on i say

---
+++ build/CMakeFiles/openGlExample.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/openGlExample.dir/main.cpp.o"
+  "CMakeFiles/openGlExample.dir/main.cpp.o.d"
+  "openGlExample"
+  "openGlExample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/openGlExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/openGlExample.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for openGlExample.
+# This may be replaced when dependencies are built.
