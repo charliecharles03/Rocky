475b9b87b9d5c8324e77d57dd5e5292e9a5271d9
charliecharles03 <cloud9stark@gmail.com>
2025-04-04 01:04:15 +0530

    please dont do anything, your actions are your biggest enemy

--- build/CMakeFiles/glad.dir/DependInfo.cmake
+++ build/CMakeFiles/glad.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/Users/anilkumarc/Documents/playground/Rocky/glad.c" "CMakeFiles/glad.dir/glad.c.o" "gcc" "CMakeFiles/glad.dir/glad.c.o.d"
+  "/home/charlie/Documents/github/Rocky/glad.c" "CMakeFiles/glad.dir/glad.c.o" "gcc" "CMakeFiles/glad.dir/glad.c.o.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
