a0eed03698affcb36f5ae2eeb0205c6da62abe24
charliecharles03 <cloud9stark@gmail.com>
2025-04-25 01:50:06 +0530

    moved code here and there a bit for foolish reasons, but hopefully it will not be helpful

--- main.cpp
+++ main.cpp
@@ -5,156 +5,187 @@
 #include "include/glad/glad.h"
 #include <GLFW/glfw3.h>  
 #include <math.h>
+#include "include/shader_s.h"
 
 const char *vertexShaderSource ="#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "layout (location = 1) in vec3 aColor;\n"
-    "out vec3 ourColor;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos, 1.0);\n"
-    "   ourColor = aColor;\n"
-    "}\0";
+"layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos, 1.0);\n"
+"   ourColor = aColor;\n"
+"}\0";
 
 const char *fragmentShaderSource = "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "in vec3 ourColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(ourColor, 1.0);\n"
-    "}\n\0";
+"out vec4 FragColor;\n"
+"in vec3 ourColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(ourColor, 1.0);\n"
+"}\n\0";
 
 
-    float myFloat = 0.5f;
+float myFloat = 0.5f;
+
+typedef struct{
+    unsigned int vao;
+    unsigned int vbo;
+    unsigned int ebo;
+} dummy;
+
+
+
+GLFWwindow* windowMaker(){
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 800, "dumb triangle", NULL, NULL);
+
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+    }
+    glfwMakeContextCurrent(window);
+    return window;
+}
+
+GLuint createVertexShader(){
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    return vertexShader;
+}
+
+GLuint createFragmentShader(){
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    return fragmentShader;
+}
+
+dummy bufferCreator(float vertices[], int verticesArraySize, unsigned int indices[], int indicesArraySize){
+    dummy buffers;
+
+    glGenVertexArrays(1, &buffers.vao);
+    glGenBuffers(1, &buffers.vbo);
+    glGenBuffers(1, &buffers.ebo);
+    glBindVertexArray(buffers.vao);
+
+    glBindBuffer(GL_ARRAY_BUFFER,buffers.vbo);
+    glBufferData(GL_ARRAY_BUFFER, verticesArraySize , vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffers.ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indicesArraySize ,indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+    return buffers;
+}
+
+
 int main()
 {
-	glfwInit();
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	GLFWwindow* window = glfwCreateWindow(800, 800, "dumb triangle", NULL, NULL);
-	if (window == NULL)
-	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
+    // creating GLFW window 
+    GLFWwindow* window = windowMaker();
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cerr << "Failed to initialize GLAD\n";
         return -1;
     }
-	glViewport(0, 0, 800, 800);
+    glViewport(0, 0, 800, 800);
+
+    Shader ourShader("3.3.shader.vs", "3.3.shader.fs"); 
+
+    GLuint vertexShader = createVertexShader();
+    GLuint fragmentShader = createFragmentShader();
 
 
-	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
 
-	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-
-	GLuint shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 
     float vertices[] = {
-        // positions         // colors
-        0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
-        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
-        0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
+        0.0f,  0.0f, 0.0f,  0.0f, 0.0f, 1.0f,      //0
+        +0.5f, +0.5f, 0.0f,  0.0f, 1.0f, 0.0f,    // 2
+        -0.5f, +0.5f, 0.0f,  1.0f, 0.0f, 0.0f,    //1
+        -0.5f,  -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,    //3
+        +0.5f,  -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,    //4
     };   
 
-    unsigned int indices[] = {  // note that we start from 0!
-        0, 1, 3,  // first Triangle
-        1, 2, 3   // second Triangle
+    unsigned int indices[] = {  
+        0, 1, 2,  
+        1, 0, 4   
     };
 
-    unsigned int VBO, VAO, EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-    glBindBuffer(GL_ARRAY_BUFFER, 0); 
-
+    dummy bufferResponse =  bufferCreator(vertices,sizeof(vertices),indices,sizeof(indices));
 
     glBindVertexArray(0); 
-
     double previousTime = glfwGetTime();
     int frameCount = 0;
 
-    // Setup Dear ImGui context
+
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO &io = ImGui::GetIO();
-    // Setup Platform/Renderer bindings
+
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 330");
-    // Setup Dear ImGui style
+
     ImGui::StyleColorsDark();
 
 
 
     while (!glfwWindowShouldClose(window))
-	{
-		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+    {
+        glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // imgui
-        // feed inputs to dear imgui, start new frame
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame(); 
 
         glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
+        glBindVertexArray(bufferResponse.vao);
 
-        glDrawArrays(GL_TRIANGLES,0,3);
 
-        // Setup
+        glDrawArrays(GL_TRIANGLES,0,6);
+
         const int NUM_SAMPLES = 100;
         static float fpsSamples[NUM_SAMPLES] = {};
         static int fpsIndex = 0;
 
-        // Each frame
         fpsSamples[fpsIndex] = ImGui::GetIO().Framerate;
         fpsIndex = (fpsIndex + 1) % NUM_SAMPLES;
 
-        // Calculate average
         float fpsSum = 0.0f;
         for (int i = 0; i < NUM_SAMPLES; ++i) {
             fpsSum += fpsSamples[i];
         }
         float avgFps = fpsSum / NUM_SAMPLES;
 
-        // render your GUI
         ImGui::Begin("Demo window");
         ImGui::PlotLines("FPS Graph", fpsSamples, NUM_SAMPLES, 0, nullptr, 0.0f, 144.0f, ImVec2(0, 60));
         ImGui::Text("FPS number %f",avgFps);
         ImGui::End();
 
-        // Render dear imgui into screen
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
@@ -167,9 +198,10 @@ int main()
     ImGui::DestroyContext();
 
 
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-	glDeleteProgram(shaderProgram);
-	glfwDestroyWindow(window);
-	glfwTerminate(); return 0;
+    glDeleteVertexArrays(1, &bufferResponse.vao);
+    glDeleteBuffers(1, &bufferResponse.vbo);
+    glDeleteBuffers(1,&bufferResponse.ebo);
+    glDeleteProgram(shaderProgram);
+    glfwDestroyWindow(window);
+    glfwTerminate(); return 0;
 }
